e2579d1a3aaeb689d47120111fac762df7b87cee
pl1025449 <pl1025449@ahschool.com>
2026-04-21 07:47:40 -0400

    Add pop_up.py for displaying messages from server

---
+++ pop_up.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import requests
+import time
+
+PI_URL = "http://192.168.240.123:5000/popup_status"
+
+root = tk.Tk()
+root.title("messagebox")
+root.geometry("300x150")
+root.attributes('-topmost', True)
+root.withdraw()  # hide first
+
+label = tk.Label(root, text="We are not alone! ")
+label.pack(expand=True)
+
+def check_popup():
+    try:
+        data = requests.get(PI_URL, timeout=1).json()
+        if data.get("should_popup"):
+            root.deiconify()  # show
+        else:
+            root.withdraw()  # hide
+    except:
+        pass
+    root.after(500, check_popup)  # checks every 500milisec
+
+check_popup()
+root.mainloop()
